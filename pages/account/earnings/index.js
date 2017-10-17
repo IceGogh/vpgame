972152781e0e6cb3e6a5ef9cf133d6d4fac79dc5
Gogh-office <4535292@qq.com>
2017-10-17 11:52:31 +0800

    my match data  drawing the canvas ..

--- pages/account/earnings/index.js
+++ pages/account/earnings/index.js
@@ -34,7 +34,40 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    //  canvas 绘制画布
+    var cvs1 = wx.createCanvasContext('weekCanvas');
+        // cvs2 = wx.createCanvasContext('totalCanvas');
+    
+    // 设置线条颜色
+    cvs1.setStrokeStyle("#eeeeee");
+
+    // 原点位置 canvas的边长/2  54像素单位 = 108rpx
+    cvs1.translate(54, 54);
+
+    // 线条宽度 
+    cvs1.setLineWidth(3);
+
+    // 第一遍圆圈
+    cvs1.beginPath();
+
+    // arc(x, y, 半径， 开始位置， 结束位置， false默认顺时针运动)
+    cvs1.arc(0, 0, 52, 0, 2* Math.PI, false);
+    cvs1.closePath();
+    // 描绘出点的路径
+    cvs1.stroke();
+
+    // 百分比呈现 绘制第二层(百分比)圆
+    cvs1.setStrokeStyle("#ee7142");
+    cvs1.rotate(Math.PI/-2);  // 旋转回开始位置为12点
+    cvs1.setLineWidth(3);
+    
+    cvs1.beginPath();
+    // 50% 半圆
+    cvs1.arc(0, 0, 52, 0,  Math.PI, false);
+    cvs1.stroke();
+    cvs1.closePath();   // 首尾不相连(封闭画法) 先 stroke 后 closePath
+    cvs1.draw();
+    
   },
 
   /**
